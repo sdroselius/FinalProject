6c5bc4f607565ef903715315b173f4479fef3ff0
Rob Roselius <roselius@skilldistillery.com>
2021-04-20 13:48:19 -0600

    Stub angular project, add @CrossOrigin

--- PetFindr/src/main/java/com/skilldistillery/petfindr/controllers/UserController.java
+++ PetFindr/src/main/java/com/skilldistillery/petfindr/controllers/UserController.java
@@ -1,12 +1,14 @@
 package com.skilldistillery.petfindr.controllers;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.skilldistillery.petfindr.services.UserService;
 
+@CrossOrigin({ "*", "http://localhost:4290" })
 @RequestMapping("api")
 @RestController
 public class UserController {
