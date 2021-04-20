ea824a76d3734ecd84b9c9a1ebe5734e6d6553cd
Rob Roselius <roselius@skilldistillery.com>
2021-04-20 11:05:33 -0600

    Create Boot/REST project

---
+++ PetFindr/src/main/java/com/skilldistillery/petfindr/PetFindrApplication.java
@@ -0,0 +1,13 @@
+package com.skilldistillery.petfindr;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PetFindrApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PetFindrApplication.class, args);
+	}
+
+}
