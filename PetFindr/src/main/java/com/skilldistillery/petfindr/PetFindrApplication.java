78e2a2ccba43bab9fe4afde4d2992455ee0f163b
Rob Roselius <roselius@skilldistillery.com>
2021-04-26 10:49:25 -0600

    Configure Boot for WAR deployment

--- PetFindr/src/main/java/com/skilldistillery/petfindr/PetFindrApplication.java
+++ PetFindr/src/main/java/com/skilldistillery/petfindr/PetFindrApplication.java
@@ -2,9 +2,16 @@ package com.skilldistillery.petfindr;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class PetFindrApplication {
+public class PetFindrApplication extends SpringBootServletInitializer {
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(PetFindrApplication.class);
+    }
 
 	public static void main(String[] args) {
 		SpringApplication.run(PetFindrApplication.class, args);
