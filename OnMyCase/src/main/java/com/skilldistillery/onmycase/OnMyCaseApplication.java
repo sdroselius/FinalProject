d0c03b4904fb9cf26cc067e0833d4ffbd1cdf91e
Rob Roselius <roselius@skilldistillery.com>
2024-03-05 10:43:25 -0700

    Create Spring Boot REST project, connect JPA project dependency

---
+++ OnMyCase/src/main/java/com/skilldistillery/onmycase/OnMyCaseApplication.java
@@ -0,0 +1,13 @@
+package com.skilldistillery.onmycase;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnMyCaseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OnMyCaseApplication.class, args);
+	}
+
+}
